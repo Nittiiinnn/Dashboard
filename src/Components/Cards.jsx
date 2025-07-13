50e8747c65b1450364264b9e6471c4699c64e164
Nittiiinnn <baluninitin06@gmail.com>
2025-07-13 23:24:37 +0530

    files updated

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -19,7 +19,7 @@ function Cards({title,value,color,url,stats,image}) {
         >
         <div className='flex justify-center mb-4 transform group-hover:scale-110 transition-transform duration-300'>{image}</div>
         <h4  className='text-base md:text-lg text-gray-800 font-semibold group-hover:text-white dark:text-gray-100'>{title}</h4>
-        
+
         {stats ? (
             <ul className="mb-4 text-gray-600 text-sm group-hover:text-gray-100 dark:text-gray-300">
                 {Object.entries(stats).map(([label, number]) => (
